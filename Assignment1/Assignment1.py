40bc3dfad85c9edf6dfaf9f4838d93ba2fc353c2
TomLaverghetta <46882694+TomLaverghetta@users.noreply.github.com>
2020-09-14 21:00:36 -0400

    Update Assignment1.py

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -1,4 +1,4 @@
-
+from copy import deepcopy 
 
 class Node:
     state = []
@@ -21,55 +21,68 @@ def Goal(state):
     sum = 0
     for r in range(len(state)):
         for c in range(len(state[0])):
-            sum = sum + state[r,c] 
-    return sum == 1 # TRUE if final state
+            sum = sum + state[r][c] 
+            if sum > 1:
+                return False
+    return True # TRUE if final state
 
 # Successor determines naighboring states
 def Successor(state):
     neighboringStates = []
     for r in range(len(state)):
         for c in range(len(state[0])):
-            if (state[r,c] != 0):
+            if (state[r][c] != 0):
                 # Checking Above
-                if ((r+2) < len(state) and state[r+2, c] == 0):
+                if ((r+2) < len(state) and state[r+2][c] == 0 and state[r+1][c] == 1):
                     newNode = Node()
-                    newNode.state = state
-                    newNode.state[r+2, c] = 1
-                    newNode.state[r+1, c] = newNode.state[r, c] = 0
+                    newNode.state = deepcopy(state)
+                    newNode.state[r+2][c] = 1
+                    newNode.state[r+1][c] = newNode.state[r][c] = 0
                     neighboringStates.append(newNode)
 
                 # Checking Below
-                if ((r-2) >= 0 and state[r-2, c] == 0):
+                if ((r-2) >= 0 and state[r-2][c] == 0 and state[r-1][c] == 1):
                     newNode = Node()
-                    newNode.state = state
-                    newNode.state[r-2, c] = 1
-                    newNode.state[r-1, c] = newNode.state[r, c] = 0
+                    newNode.state = deepcopy(state)
+                    newNode.state[r-2][c] = 1
+                    newNode.state[r-1][c] = newNode.state[r][c] = 0
                     neighboringStates.append(newNode)
                 
                 # Checking to the right
-                if ((c+2) < len(state[0]) and state[r, c+2] == 0):
+                if ((c+2) < len(state[0]) and state[r][c+2] == 0 and state[r][c+1] == 1):
                     newNode = Node()
-                    newNode.state = state
-                    newNode.state[r, c+2] = 1
-                    newNode.state[r, c+1] = newNode.state[r, c] = 0
+                    newNode.state = deepcopy(state)
+                    newNode.state[r][c+2] = 1
+                    newNode.state[r][c+1] = newNode.state[r][c] = 0
                     neighboringStates.append(newNode)
 
                 # checking to the left
-                if ((c-2) >= 0 and state[r, c-2] == 0):
+                if ((c-2) >= 0 and state[r][c-2] == 0 and state[r][c-1] == 1):
                     newNode = Node()
-                    newNode.state = state
-                    newNode.state[r, c-2] = 1
-                    newNode.state[r, c-1] = newNode.state[r, c] = 0
+                    newNode.state = deepcopy(state)
+                    newNode.state[r][c-2] = 1
+                    newNode.state[r][c-1] = newNode.state[r][c] = 0
                     neighboringStates.append(newNode)
     
     return neighboringStates
 
+def StateSum(state):
+    sum = 0
+    for r in range(len(state)):
+        for c in range(len(state[0])):
+            sum = sum + state[r][c]
+
+    return sum 
+
 # input is a node
 def Breadth_First_Search(InitNode):
+    index = StateSum(InitNode.state) - 1
+    closed = [[]]
     queue = []
     queue.append(InitNode)
+    # closed[0].append(InitNode.state)
     while len(queue) > 0:
-        c = queue.pop()         # Get next element of queue
+        c = queue.pop(0)         # Get next element of queue
         if Goal(c.state):       # if current state is goal
             # Find the nodePath to solution
             nodePath = []
@@ -83,10 +96,23 @@ def Breadth_First_Search(InitNode):
         else:
             neighbors = Successor(c.state)
             for n in neighbors:
-                if n.f == -1:   # unvisited
-                    n.f = 1     # mark as visited
+                unvisited = True
+                sumIndex = index - StateSum(n.state)
+                if sumIndex < len(closed):
+                    for j in range(len(closed[sumIndex])):
+                        if closed[sumIndex][j] == n.state:
+                            unvisited = False
+                            break
+                    if unvisited:   # unvisited
+                        closed[sumIndex].append(n.state)
+                        n.Prestate = c.state # The previous state (used to find state path)
+                        queue.append(n) # add to queue
+                else:
+                    closed.append([n.state])
                     n.Prestate = c.state # The previous state (used to find state path)
-                    queue.append(n) # add to queue
+                    queue.append(n) # add to queue 
+    
+    return False
     
 # input is a node
 def Depth_First_Search(InitNode):
@@ -115,7 +141,7 @@ def Greedy_Best_Search(InitNode):
     Open.append(InitNode)
     while len(Open) > 0:
         # Pop the top
-        N = Open.pop()
+        N = Open.pop(0)
         Closed.append(N)
 
         if Goal(N.state):
@@ -146,13 +172,7 @@ def Greedy_Best_Search(InitNode):
     
     return False
 
-def StatesEqual(state1, state2):
-    for r in range(len(state1)):
-        for c in range(len(state1[0])):
-            if state1[r,c] != state2[r,c]:
-                return False
-    
-    return True
+
 
 def A_Star_Search(InitNode):
     Open = []
@@ -160,7 +180,7 @@ def A_Star_Search(InitNode):
     Open.append(InitNode)
 
     while len(Open) > 0:
-        q = Open.pop() 
+        q = Open.pop(0) 
         neighbors = Successor(q.state)
         for n in neighbors:
             if Goal(n):
@@ -189,5 +209,10 @@ def A_Star_Search(InitNode):
                     
             
 
-def if __name__ == "__main__":
-    
+if __name__ == "__main__":
+    InitNode = Node()
+
+    InitNode.state = [[1,1,1,1,1,1],[1,0,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1]]
+
+    path = Breadth_First_Search(InitNode)
+    print(path)
