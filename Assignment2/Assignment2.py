e7fc97b2be914908000f14c078db7ca8f22481b4
TomLaverghetta <46882694+TomLaverghetta@users.noreply.github.com>
2020-10-04 18:05:31 -0400

    started

---
+++ Assignment2/Assignment2.py
@@ -0,0 +1,17 @@
+import enum
+
+class RGB(enum.Enum):
+    R = 0
+    G = 1
+    B = 2
+
+class Vector2D:
+    x = 0
+    y = 0
+
+class Triangle:
+    p1 = Vector2D()
+    p2 = Vector2D()
+    p3 = Vector2D()
+    RGB = 
+
